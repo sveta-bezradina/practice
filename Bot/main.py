86f854098414b8bc585a9f60a7fc639ea6c34f8c
sveta-bezradina <bezradina514@gmail.com>
2023-06-18 20:11:22 +0300

    Delete file

--- Bot/main.py
+++ Bot/main.py
@@ -1,14 +1,14 @@
 import os
 import datetime
 import requests
-import math, config
+import math
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor 
 
 #для защиты от спама
 PROXY_URL="http://proxy.server:3128"
-bot = Bot(token='TOKEN')
+bot = Bot(token='6058928493:AAHQ5s7q3opedxwqIXBJytcW3MpMN0OyBn0')
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
